fa0b1dd8d585f9d60773a7552571a1e6f4ab04d2
Jason <jsum@ancoris.com>
2023-12-16 23:03:10 +0000

    initial commit: Jetpack compose 1

---
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.recipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Column (
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Hey look some text")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {}) {
+                    Text(text = "A Button")
+                }
+            }
+        }
+    }
+}
